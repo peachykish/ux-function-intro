84dc13ad99dab968f2785f479a0921fd4e7c52a7
peachykish <nakisha.carroll@gmail.com>
2020-07-20 14:18:05 -0400

    first commit

--- code.js
+++ code.js
@@ -7,10 +7,10 @@
 
   Return the number 5 from this function.
 */
+console.log(getNumberFive())
 function getNumberFive() {
-  // Your code here
+  return 5
 }
-
 /* 
   -------TESTS---------------------------------------------------------------
   Run these commands to make sure you did it right. They should all be true.
@@ -26,8 +26,9 @@ console.log(getNumberFive() === 5);
 
   Multiply these two numbers and return the result.
 */
+console.log(multiply(2, 3))
 function multiply(a, b) {
-  // You code here
+  return a * b
 }
 
 /* 
@@ -50,7 +51,7 @@ console.log(multiply(0, 0) === 0);
   Add the text to the end of the message, and then return the complete string.
 */
 function concatenate(message, text) {
-  // You code here
+  return message + text;
 }
 
 /* 
@@ -73,9 +74,12 @@ console.log(concatenate("", "New text!") === "New text!");
   Otherwise, multiply the number by -1 and return it. 
 */
 function absoluteValue(number) {
-  // You code here
+if (number > 0) {
+  return number;
+} else {
+  return number * -1 
 }
-
+}
 /* 
   -------TESTS---------------------------------------------------------------
   Run these commands to make sure you did it right. They should all be true.
@@ -105,6 +109,15 @@ console.log(absoluteValue(0) === 0);
 */
 function evenOddOrZero(number) {
   // You code here
+  let result;
+  if (number === 0) {
+    result = "Zero";
+  } else if ( number % 2 === 0) {
+    result = "Even";
+  } else {
+    result = "Odd";
+  }
+  return result
 }
 
 /* 
